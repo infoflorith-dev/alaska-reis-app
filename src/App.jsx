9b58171fb9f0f6d126861f3097a43c817ca384e0
infoflorith-dev <infoflorith@gmail.com>
2026-04-08 19:22:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -573,7 +573,7 @@ export default function App() {
                 ...(day.excursions || []),
                 {
                   id: Date.now(),
-                 title: excursionForm.title.trim() || (excursionForm.files[0]?.name || "Excursie"),
+                 title: excursionForm.title.trim() || "Excursie zonder titel",
                   time: excursionForm.time.trim(),
                   note: excursionForm.note.trim(),
                   website: excursionForm.website.trim(),
