c9d26bf83f76d49d345d7b19d35f847d8a1edff2
infoflorith-dev <infoflorith@gmail.com>
2026-04-10 19:12:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -767,7 +767,7 @@ async function saveTrip(data) {
     <div style={styles.card}>
   <div style={{
   position: "relative",
-  height: 220,
+ height: 280,
   borderBottom: "1px solid #e2e8f0",
 backgroundImage: 'url("https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1600&q=80")',
   backgroundSize: "cover",
