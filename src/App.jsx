6ecb45893d04cd362e326b23ee8639377ae0ffe3
infoflorith-dev <infoflorith@gmail.com>
2026-04-11 11:51:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -790,9 +790,13 @@ selectedDay.location?.toLowerCase().includes("seattle")
   ? "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
   : selectedDay.location?.toLowerCase().includes("wrangell")
   ? "https://images.unsplash.com/photo-1469474968028-56623f02e42e"
-  : selectedDay.location?.toLowerCase().includes("sitka")
-  ? "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
-  : "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
+: selectedDay.location?.toLowerCase().includes("sitka")
+  ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+: selectedDay.location?.toLowerCase().includes("victoria")
+  ? "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+: selectedDay.location?.toLowerCase().includes("hubbard")
+  ? "https://images.unsplash.com/photo-1441974231531-c6227db76b6e"
+: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
 })`,
   backgroundSize: "cover",
   backgroundPosition: "center",
