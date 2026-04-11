9ccd15f4aff0363363d682b51012ec864598197d
infoflorith-dev <infoflorith@gmail.com>
2026-04-11 12:00:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -794,8 +794,8 @@ selectedDay.location?.toLowerCase().includes("seattle")
   ? "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
 : selectedDay.location?.toLowerCase().includes("victoria")
   ? "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
- selectedDay.location?.toLowerCase().includes("golden") ||
-selectedDay.location?.toLowerCase().includes("banff")
+: selectedDay.location?.toLowerCase().includes("golden") ||
+  selectedDay.location?.toLowerCase().includes("banff")
   ? "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
   ? "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429"
 : selectedDay.location?.toLowerCase().includes("banff")
