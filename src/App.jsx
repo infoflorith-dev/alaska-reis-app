1d2873d87f4fbd7edbaea1f67a2e3c16603a71f3
infoflorith-dev <infoflorith@gmail.com>
2026-04-11 12:24:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -792,13 +792,12 @@ selectedDay.location?.toLowerCase().includes("seattle")
   ? "https://images.unsplash.com/photo-1469474968028-56623f02e42e"
   : selectedDay.location?.toLowerCase().includes("sitka")
   ? "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee"
- : selectedDay.location?.toLowerCase().includes("victoria")
+  : selectedDay.location?.toLowerCase().includes("victoria")
   ? "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
-  : selectedDay.location?.toLowerCase().includes("yellowstone")
-  ? "https://images.unsplash.com/photo-1501594907352-04cda38ebc29"
-  ? "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
   : selectedDay.location?.toLowerCase().includes("hubbard")
   ? "https://images.unsplash.com/photo-1441974231531-c6227db76b6e"
+  : selectedDay.location?.toLowerCase().includes("yellowstone")
+  ? "https://images.unsplash.com/photo-1501594907352-04cda38ebc29"
   : selectedDay.location?.toLowerCase().includes("golden") ||
     selectedDay.location?.toLowerCase().includes("banff")
   ? "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
